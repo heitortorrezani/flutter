4079215489fc6b92ae72c21be8d0ad9e63b60247
Heitor <heitortorrezani@gmail.com>
2026-03-19 17:42:59 -0300

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1194023714"
+    serviceWorkerVersion: "459144112"
   }
 });
